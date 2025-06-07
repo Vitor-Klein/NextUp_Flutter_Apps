8c603d0952f1c1fe6318c8d1e334c21bdc1d6f7c
Vitor-Klein <vitorhklein2@gmail.com>
2025-06-07 10:06:14 -0300

    start use youtube_player_iframe

--- harold_lefty_williams/lib/pages/videos_page.dart
+++ harold_lefty_williams/lib/pages/videos_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class VideosPage extends StatefulWidget {
   const VideosPage({super.key});
@@ -9,36 +9,89 @@ class VideosPage extends StatefulWidget {
 }
 
 class _VideosPageState extends State<VideosPage> {
-  late final WebViewController _controller;
-  bool _isLoading = true;
+  late YoutubePlayerController _controller;
+
+  final List<String> videoIds = ['85yBqGwOV2k', 'I8vRFb-ZP8k', 'Fb9-x9BCBS0'];
+
+  int currentIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageFinished: (_) {
-            setState(() => _isLoading = false);
-          },
-        ),
-      )
-      ..loadRequest(
-        Uri.parse('https://www.youtube.com/dare2dreamfamily?app=desktop'),
-      );
+
+    _controller = YoutubePlayerController(
+      params: const YoutubePlayerParams(
+        showControls: true,
+        showFullscreenButton: true,
+      ),
+    );
+
+    // Carrega o primeiro vídeo
+    _loadCurrentVideo();
+  }
+
+  void _loadCurrentVideo() {
+    _controller.loadVideoById(videoId: videoIds[currentIndex]);
+  }
+
+  void _playNext() {
+    if (currentIndex < videoIds.length - 1) {
+      setState(() {
+        currentIndex++;
+        _loadCurrentVideo();
+      });
+    }
+  }
+
+  void _playPrevious() {
+    if (currentIndex > 0) {
+      setState(() {
+        currentIndex--;
+        _loadCurrentVideo();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Dare 2 Dream - YouTube')),
-      body: Stack(
-        children: [
-          WebViewWidget(controller: _controller),
-          if (_isLoading) const Center(child: CircularProgressIndicator()),
-        ],
-      ),
+    return YoutubePlayerScaffold(
+      controller: _controller,
+      builder: (context, player) {
+        return Scaffold(
+          appBar: AppBar(title: const Text('Dare 2 Dream - Vídeos')),
+          body: Column(
+            children: [
+              Expanded(child: player),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: currentIndex > 0 ? _playPrevious : null,
+                    icon: const Icon(Icons.arrow_back),
+                    label: const Text('Anterior'),
+                  ),
+                  Text('${currentIndex + 1} / ${videoIds.length}'),
+                  ElevatedButton.icon(
+                    onPressed: currentIndex < videoIds.length - 1
+                        ? _playNext
+                        : null,
+                    icon: const Icon(Icons.arrow_forward),
+                    label: const Text('Próximo'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 100),
+            ],
+          ),
+        );
+      },
     );
   }
 }
