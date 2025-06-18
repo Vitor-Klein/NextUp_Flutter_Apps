0060ef0a96a399ceb8e3686758e275d39266345e
Vitor-Klein <vitorhklein2@gmail.com>
2025-06-18 09:45:13 -0300

    fix swizzing on luke lezon

--- stag_support/ios/Runner/AppDelegate.swift
+++ stag_support/ios/Runner/AppDelegate.swift
@@ -1,13 +1,37 @@
-import Flutter
 import UIKit
+import Flutter
+import Firebase
+import FirebaseMessaging
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
+
+    UNUserNotificationCenter.current().delegate = self
+    Messaging.messaging().delegate = self
+
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(
+      options: authOptions,
+      completionHandler: { _, _ in })
+
+    application.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+      willPresent notification: UNNotification,
+      withCompletionHandler completionHandler:
+         @escaping (UNNotificationPresentationOptions) -> Void) {
+    completionHandler([[.banner, .sound]])
+  }
+
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("FCM Token: \(fcmToken ?? "")")
   }
 }
