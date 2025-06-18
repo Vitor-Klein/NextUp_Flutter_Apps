a9584234c1edf4ab341d8ef7d6b9be240a3db55f
Vitor-Klein <vitorhklein2@gmail.com>
2025-06-18 11:16:43 -0300

    fiz share message

--- harold_lefty_williams/lib/pages/home_page.dart
+++ harold_lefty_williams/lib/pages/home_page.dart
@@ -85,7 +85,7 @@ class _HomePageState extends State<HomePage> {
         ? 'https://play.google.com/store/apps/details?id=com.seuapp.android'
         : 'https://apps.apple.com/app/id0000000000';
 
-    final mensagem = 'Confira este app incrível! Baixe agora:\n$link';
+    final mensagem = 'Check out this amazing app! Download now:\n$link';
 
     await SharePlus.instance.share(ShareParams(text: mensagem));
   }
