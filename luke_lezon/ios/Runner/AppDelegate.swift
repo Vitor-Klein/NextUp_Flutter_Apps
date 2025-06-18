7bbe623edea743770e0b7fb5a850dbed31b93532
Vitor-Klein <vitorhklein2@gmail.com>
2025-06-18 10:21:29 -0300

    remove configs on stag support and fiz luke lezon

--- luke_lezon/ios/Runner/AppDelegate.swift
+++ luke_lezon/ios/Runner/AppDelegate.swift
@@ -1,18 +1,37 @@
 import UIKit
 import Flutter
 import Firebase
-import UserNotifications
+import FirebaseMessaging
 
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@main
+@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
 
     UNUserNotificationCenter.current().delegate = self
+    Messaging.messaging().delegate = self
 
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(
+      options: authOptions,
+      completionHandler: { _, _ in })
+
+    application.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+      willPresent notification: UNNotification,
+      withCompletionHandler completionHandler:
+         @escaping (UNNotificationPresentationOptions) -> Void) {
+    completionHandler([[.banner, .sound]])
+  }
+
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("FCM Token: \(fcmToken ?? "")")
   }
 }
--- stag_support/ios/Runner/AppDelegate.swift
+++ stag_support/ios/Runner/AppDelegate.swift
@@ -1,37 +1,13 @@
-import UIKit
 import Flutter
-import Firebase
-import FirebaseMessaging
+import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
-
-    UNUserNotificationCenter.current().delegate = self
-    Messaging.messaging().delegate = self
-
-    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-    UNUserNotificationCenter.current().requestAuthorization(
-      options: authOptions,
-      completionHandler: { _, _ in })
-
-    application.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func userNotificationCenter(_ center: UNUserNotificationCenter,
-      willPresent notification: UNNotification,
-      withCompletionHandler completionHandler:
-         @escaping (UNNotificationPresentationOptions) -> Void) {
-    completionHandler([[.banner, .sound]])
-  }
-
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-    print("FCM Token: \(fcmToken ?? "")")
   }
 }
