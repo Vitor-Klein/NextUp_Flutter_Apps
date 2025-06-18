5f8b30bf4c8cb3a43b8475c0e45cb55e731282fc
Vitor-Klein <vitorhklein2@gmail.com>
2025-06-18 09:40:35 -0300

    ..

--- harold_lefty_williams/ios/Runner/AppDelegate.swift
+++ harold_lefty_williams/ios/Runner/AppDelegate.swift
@@ -1,45 +1,13 @@
-import UIKit
 import Flutter
-import Firebase
-import FirebaseMessaging
+import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Inicializa o Firebase
-    FirebaseApp.configure()
-
-    // Registra plugins Flutter
     GeneratedPluginRegistrant.register(with: self)
-
-    // Configura notificações
-    UNUserNotificationCenter.current().delegate = self
-    Messaging.messaging().delegate = self
-
-    // Solicita permissão para notificações
-    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-    UNUserNotificationCenter.current().requestAuthorization(
-      options: authOptions,
-      completionHandler: { _, _ in })
-
-    application.registerForRemoteNotifications()
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // Mostra notificação quando app está em primeiro plano
-  func userNotificationCenter(_ center: UNUserNotificationCenter,
-      willPresent notification: UNNotification,
-      withCompletionHandler completionHandler:
-         @escaping (UNNotificationPresentationOptions) -> Void) {
-    completionHandler([[.banner, .sound]])
-  }
-
-  // Captura o token do FCM quando atualizado
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-    print("🔥 Novo FCM Token: \(fcmToken ?? "nenhum")")
   }
 }
