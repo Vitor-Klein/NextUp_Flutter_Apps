b307c29f44994f5e3b1f27f5e67bd0975bc5e570
Vitor-Klein <vitorhklein2@gmail.com>
2025-07-21 11:04:16 -0300

    start polices app

---
+++ florence_police/lib/pages/home_page.dart
@@ -0,0 +1,311 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:share_plus/share_plus.dart';
+import 'dart:io';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin _localNotifications =
+      FlutterLocalNotificationsPlugin();
+
+  bool showStatus = false;
+  String statusImageUrl = '';
+  String statusLinkUrl = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _requestNotificationPermissions();
+    _initializeFirebaseMessaging();
+    _initializeLocalNotifications();
+    _loadBannerConfig();
+  }
+
+  Future<void> _loadBannerConfig() async {
+    final remoteConfig = FirebaseRemoteConfig.instance;
+    await remoteConfig.fetchAndActivate();
+
+    setState(() {
+      showStatus = remoteConfig.getBool('show_status');
+      statusImageUrl = remoteConfig.getString('status_image_url');
+      statusLinkUrl = remoteConfig.getString('status_link_url');
+    });
+  }
+
+  void _launchBanner() async {
+    final Uri uri = Uri.parse(statusLinkUrl);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.platformDefault);
+    }
+  }
+
+  void _requestNotificationPermissions() async {
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('Permissões de notificação concedidas');
+    } else {
+      print('Permissões de notificação negadas');
+    }
+  }
+
+  void _initializeFirebaseMessaging() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Recebeu mensagem no foreground: ${message.notification?.title}');
+      _showLocalNotification(message);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Usuário abriu a notificação');
+    });
+
+    _firebaseMessaging.getToken().then((token) {
+      print('Token FCM: $token');
+    });
+  }
+
+  void _initializeLocalNotifications() {
+    const AndroidInitializationSettings androidSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const InitializationSettings initSettings = InitializationSettings(
+      android: androidSettings,
+    );
+
+    _localNotifications.initialize(initSettings);
+  }
+
+  void _showLocalNotification(RemoteMessage message) {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+          'channel_id',
+          'channel_name',
+          channelDescription: 'Descrição do canal',
+          importance: Importance.max,
+          priority: Priority.high,
+        );
+
+    const NotificationDetails platformDetails = NotificationDetails(
+      android: androidDetails,
+    );
+
+    _localNotifications.show(
+      0,
+      message.notification?.title ?? 'Sem título',
+      message.notification?.body ?? 'Sem conteúdo',
+      platformDetails,
+    );
+  }
+
+  void _compartilharApp() async {
+    final remoteConfig = FirebaseRemoteConfig.instance;
+
+    final String link = Platform.isAndroid
+        ? remoteConfig.getString('android_share_url')
+        : remoteConfig.getString('ios_share_url');
+
+    final String mensagem = 'Check out this amazing app! Download now:\n$link';
+
+    await SharePlus.instance.share(ShareParams(text: mensagem));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox.expand(
+            child: Image.asset('assets/background.jpg', fit: BoxFit.cover),
+          ),
+          SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      PopupMenuButton<String>(
+                        icon: const Icon(
+                          Icons.more_vert,
+                          size: 28,
+                          color: Colors.white,
+                        ),
+                        color: Colors.white,
+                        onSelected: (value) {
+                          Navigator.pushNamed(context, value);
+                        },
+                        itemBuilder: (BuildContext context) => [
+                          PopupMenuItem<String>(
+                            value: '/messages',
+                            child: Row(
+                              children: const [
+                                Icon(Icons.message, color: Colors.black),
+                                SizedBox(width: 8),
+                                Text(
+                                  'Messages',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 250),
+                GestureDetector(
+                  onTap: () async {
+                    final Uri telUri = Uri(scheme: 'tel', path: '988');
+                    if (await canLaunchUrl(telUri)) {
+                      await launchUrl(telUri);
+                    } else {
+                      print('Não foi possível iniciar a chamada.');
+                    }
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 2),
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      image: const DecorationImage(
+                        image: AssetImage('assets/A_Button.png'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: _ImageButton(
+                            label: "",
+                            imageAsset: 'assets/About.png',
+                            onTap: () {
+                              Navigator.pushNamed(context, '/about');
+                            },
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: _ImageButton(
+                            label: "",
+                            imageAsset: 'assets/Resources.png',
+                            onTap: () {
+                              Navigator.pushNamed(context, '/resources');
+                            },
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: _ImageButton(
+                            label: "",
+                            imageAsset: 'assets/Share.png',
+                            onTap: _compartilharApp,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                // BANNER CONTROLADO POR REMOTE CONFIG
+                const SizedBox(height: 10),
+                if (showStatus && statusImageUrl.isNotEmpty)
+                  GestureDetector(
+                    onTap: _launchBanner,
+                    child: Container(
+                      height: 100,
+                      width: double.infinity,
+                      margin: const EdgeInsets.symmetric(horizontal: 0),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(statusImageUrl),
+                          fit: BoxFit.contain,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ImageButton extends StatelessWidget {
+  final String label;
+  final String imageAsset;
+  final VoidCallback onTap;
+
+  const _ImageButton({
+    required this.label,
+    required this.imageAsset,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            height: 140,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(imageAsset),
+                fit: BoxFit.contain,
+              ),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  offset: const Offset(4, 10),
+                  blurRadius: 6,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            label,
+            style: const TextStyle(
+              fontSize: 14,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
