f53918873ecf1ef6ed8e19e73d49dbf00aa2fbcb
Vitor-Klein <vitorhklein2@gmail.com>
2025-08-18 11:19:49 -0300

    fizx link

--- luke_lezon/lib/pages/books_page.dart
+++ luke_lezon/lib/pages/books_page.dart
@@ -9,8 +9,7 @@ class BooksPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return WebViewPage(
       title: 'Books',
-      url:
-          'https://www.amazon.com/stores/Luke-Lezon/author/B07TV95H29?language=es&isDramIntegra[…]portalEnabled=true&ccs_id=87a85b1e-cc28-4fac-b323-2e34fa567468',
+      url: 'https://www.amazon.com/stores/Luke-Lezon/author/B07TV95H29',
     );
   }
 }
